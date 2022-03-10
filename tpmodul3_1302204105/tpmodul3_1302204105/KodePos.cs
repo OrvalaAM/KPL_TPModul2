f8da985aa951b406bfec9de2a6cb7c34bbf9d7f2
OrvalaAM <90945844+OrvalaAM@users.noreply.github.com>
2022-03-10 22:32:19 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul3_1302204105/tpmodul3_1302204105/KodePos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_1302204105
+{
+    internal class KodePos
+    {
+        string[] kelurahan = {"Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura",
+        "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja"};
+        string[] kodepos = {"40266", "40287", "40267", "40256", "40287",
+        "40286", "40286", "40286", "40272", "40274", "40273"};
+
+        public void getKodePos()
+        {
+            int panjang = kodepos.Length;
+            for(int i = 0; i < panjang; i++)
+            {
+                Console.WriteLine(kodepos[i]);
+            }
+        }
+    }
+}
